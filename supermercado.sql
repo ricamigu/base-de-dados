28886bd40d884d346bfe7ba676a12da821a31d3b
ricamigu <38142658+ricamigu@users.noreply.github.com>
2020-04-21 03:10:38 +0100

    Add files via upload

--- supermercado.sql
+++ supermercado.sql
@@ -84,11 +84,12 @@ CREATE TABLE `FUNCIONARIO` (
   `Nome` varchar(45) NOT NULL,
   `NumCC` int(12) NOT NULL,
   `DataNascimento` date NOT NULL,
-  `Supervisor` int(11) DEFAULT NULL,
   `Morada` varchar(45) NOT NULL,
-  `NumTelemovel` varchar(9) NOT NULL,
+  
   `Email` varchar(45) DEFAULT NULL,
   `Genero` ENUM('M', 'F') NOT NULL,
+  `Departamento` varchar(45) NOT NULL,
+  `Supervisor` int(11) DEFAULT NULL,
   `Salário` int(11) NOT NULL,
   `HorasTrabalho` int(11) NOT NULL,
   PRIMARY KEY (`IdFuncionario`),
@@ -106,6 +107,30 @@ LOCK TABLES `FUNCIONARIO` WRITE;
 /*!40000 ALTER TABLE `Funcionarios` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Funcionarios` ENABLE KEYS */;
 UNLOCK TABLES;
+
+-- num telefone para multi-valor
+
+DROP TABLE IF EXISTS `NUM_TELEFONE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `NUM_TELEFONE` (
+  `IdFuncionario` int NOT NULL,
+  `Num_Telemovel` int NOT NULL,
+  PRIMARY KEY (`IdFuncionario`,`Num_Telemovel`),
+  FOREIGN KEY(`IdFuncionario`) REFERENCES `FUNCIONARIO`(`IdFuncionario`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Produtos`
+--
+
+LOCK TABLES `NUM_TELEFONE` WRITE;
+/*!40000 ALTER TABLE `Produtos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
 
 --
 -- Table structure for table `Produtos`
@@ -133,6 +158,29 @@ LOCK TABLES `PRODUTO` WRITE;
 /*!40000 ALTER TABLE `Produtos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Produtos` ENABLE KEYS */;
 UNLOCK TABLES;
+
+-- DEPARTAMENTO
+
+DROP TABLE IF EXISTS `DEPARTAMENTO`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DEPARTAMENTO` (
+  `NomeDepartamento` varchar(45) NOT NULL,
+  `Gestor` int NOT NULL,
+  PRIMARY KEY (`NomeDepartamento`),
+  FOREIGN KEY(`Gestor`) REFERENCES `FUNCIONARIO`(`IdFuncionario`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DEPARTAMENTO`
+--
+
+LOCK TABLES `DEPARTAMENTO` WRITE;
+/*!40000 ALTER TABLE `Produtos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
